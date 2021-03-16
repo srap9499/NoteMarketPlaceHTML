dacbc3a50e11b9019368b564995bf4c2e7770b67
Siddhesh Panchal <59734506+srap9499@users.noreply.github.com>
2021-03-16 21:44:23 +0530

    First Commit

---
+++ front/js/script.js
@@ -0,0 +1,91 @@
+/* ====================================================
+                Navigation
+==================================================== */
+
+/* Show & Hide White Navigation */
+
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            //show white nav
+            $('nav').addClass("white-nav-top");
+
+            // Show dark logo
+            $('.navbar-brand img').attr("src", "images/pre-login/logo.png");
+        } else {
+
+            //hide white nav
+            $('nav').removeClass("white-nav-top");
+
+            // Show logo
+            $('.navbar-brand img').attr("src", "images/pre-login/top-logo.png");
+        }
+    }
+
+});
+
+// Smoothing Scrolling
+$(function () {
+
+    $('a.smooth-scroll').click(function (event) {
+
+        event.preventDefault();
+
+        // get section id like #about, #services, #work, #team and etc.
+        var section_id = $(this).attr('href');
+
+        $('html,body').animate({
+            scrollTop: $(section_id).offset().top - 64
+        }, 1250, 'easeInOutExpo');
+
+    });
+
+});
+
+/* ====================================================
+                Accodion
+==================================================== */
+$(document).ready(function() {
+    
+    for ( let i = 1; i <= 7; i++ ) {
+        $(".showdata" + i).click(function() {
+            $(".myhead" + i).hide();
+            $(".mybody" + i).show();
+        });
+        $(".hidedata" + i).click(function() {
+            $(".myhead" + i).show();
+            $(".mybody" + i).hide();
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
